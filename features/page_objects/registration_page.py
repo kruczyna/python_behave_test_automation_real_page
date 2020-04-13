b57cb9a157e9a6d8acd5f01a562d1e5914dfce91
kruczynakruka <victoria.kruczek@stxnext.pl>
2020-04-13 17:13:15 +0200

    VIP Add register scenarios

---
+++ features/page_objects/registration_page.py
@@ -0,0 +1,59 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+
+from features.page_objects.base_page import BasePage
+
+
+class RegistrationPage(BasePage):
+    register_button = (By.CSS_SELECTOR, '.page-login__register-button')
+    email_field = (By.CSS_SELECTOR, 'input[type="email"]')
+    email_field_error = (By.CSS_SELECTOR, 'input[type="email"] + span.validation__message')
+    password_field = (By.CSS_SELECTOR, 'input[type="password"]')
+    password_field_error = (By.CSS_SELECTOR, 'input[type="password"] + span.validation__message')
+    terms_and_conditions_agreement_checkbox = (By.CSS_SELECTOR, 'div[data-key="acceptTerms"]')
+    age_requirements_agreement_checkbox = (By.CSS_SELECTOR, 'div[data-key="acceptAgeRequirements"]')
+    sensitive_data_agreement_checkbox = (By.CSS_SELECTOR, 'div[data-key="sensitiveAccepted"]')
+    marketing_agreement_checkbox = (By.CSS_SELECTOR, 'data-key="marketingAccepted"')
+    create_button = (By.CSS_SELECTOR, '.create-new-account-page__register-button')
+
+    @staticmethod
+    def get_agreement_error(agreement_selector):
+        return By.CSS_SELECTOR, f"{agreement_selector}[class*='checkbox--validation-error']"
+
+    def navigate_to_register_form(self) -> None:
+        self.visit()
+        self.find_element(*self.login_link).click()
+        assert "login" in self.get_current_url()
+        self.find_element(*self.register_button).click()
+        self.wait_for_element(self.password_field)
+        self.wait_for_element(self.email_field)
+        assert "create-account" in self.get_current_url()
+
+    def submit_empty_register_form(self) -> None:
+        self.find_element(*self.rodo_button).click()
+        self.find_element(*self.create_button).click()
+
+    def get_required_register_fields(self) -> None:
+        self.find_element(*self.email_field_error)
+        self.find_element(*self.password_field_error)
+        self.get_agreement_error(self.age_requirements_agreement_checkbox)
+        self.get_agreement_error(self.sensitive_data_agreement_checkbox)
+        self.get_agreement_error(self.marketing_agreement_checkbox)
+
+    def accept_agreements_checkoboxes(self) -> None:
+        self.find_element(*self.age_requirements_agreement_checkbox).click()
+        self.find_element(*self.sensitive_data_agreement_checkbox).click()
+        self.find_element(*self.marketing_agreement_checkbox).click()
+
+    def input_invalid_register_credentials(self, email: str, password: str) -> None:
+        self.find_element(*self.email_field).send_keys(email)
+        self.find_element(*self.password_field).send_keys(password)
+
+    def get_register_form_error(self) -> None:
+        try:
+            self.wait_for_element(self.email_field)
+        except TimeoutException:
+            self.wait_for_element(self.password_field_error)
+
+    def submit_register_form(self) -> None:
+
---
+++ features/registration.feature
@@ -0,0 +1,25 @@
+Feature: Registration page
+  In order to be able to register an account
+  As a user
+  I want to fill out registration form
+
+  Background:
+    Given User navigates to registration form
+
+  Scenario: As a user I want to see which fields are required
+    When User submits an empty registration form
+    Then User sees which fields are required
+
+  Scenario: As a user I want to register an account
+    When User submits valid register credentials
+
+  Scenario Outline: As a user I want to have register form fields validated
+    When User inputs invalidly formatted "<email>" and "<password>"
+    Then User sees register form validation error
+    Examples:
+      | email               | password                                         |
+      | test                | test                                             |
+      | test@test           | 1                                                |
+      | valid@email.address | short                                            |
+      | test                | validPassword                                    |
+      | valid@email.address | 123456789012345678901234567890_too_long_password |
